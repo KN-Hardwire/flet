304e2fd2cc54c4cab39df4c9ad753ccf4a4b6652
okmarcel <>
2025-11-03 10:21:56 +0100

    change output to 1 pin

--- flute.c
+++ flute.c
@@ -17,8 +17,7 @@ typedef unsigned int uint;
 #define BUTTON7     ((uint)6)
 #define BUTTON8     ((uint)7)
 #define ADC26       ((uint)26)
-#define OUT_VOLUME  ((uint)27)
-#define OUT_FREQ    ((uint)27)
+#define OUT_PIN		((uint)27)
 
 // note frequencies
 #define NOTE_C4     261.63
@@ -40,7 +39,6 @@ typedef unsigned int uint;
 #define MIN_VOLUME 0
 #define MAX_VOLUME 4000
 #define TMP_FIXED 2000      // temp volume for testing
-#define WAVE_SLEEP_TIME 10
 #define DUTY 65535
 
 void generate_square_wave(float freq);
@@ -50,18 +48,17 @@ int main(void) {
     uint buttons[BUTTON_COUNT] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, BUTTON8};
     //bool buttons_pressed[BUTTON_COUNT] = {0};
     int volume = 0;
-    // out = syngal analogowy
 
     for (register int i = 0; i < BUTTON_COUNT; i++) {
         gpio_init(buttons[i]);
+		gpio_set_dir(buttons[i], GPIO_IN);
+		gpio_pull_up(buttons[i]);
     }
-    adc_init();
-    adc_gpio_init(ADC26);
-    adc_select_input(0);
-    gpio_set_function(OUT_VOLUME, GPIO_FUNC_PWM);
-    gpio_set_function(OUT_FREQ, GPIO_FUNC_PWM);
-
-    //uint slice = pwm_gpio_to_slice_num(OUT_VOLUME);
+    //adc_init();
+    //adc_gpio_init(ADC26);
+    //adc_select_input(0);
+	gpio_init(OUT_PIN);
+    gpio_set_function(OUT_PIN, GPIO_FUNC_PWM);
     
     while (true) {
 		uint8_t button_mask = 0;
@@ -79,13 +76,13 @@ int main(void) {
 }
 
 void generate_square_wave(float freq) {
-    uint slice_num = pwm_gpio_to_slice_num(OUT_FREQ);
+    uint slice_num = pwm_gpio_to_slice_num(OUT_PIN);
     float clk_div = (float)125000000 / freq / (DUTY + 1);
     if (clk_div < 1) clk_div = 1;
     
     pwm_set_clkdiv(slice_num, clk_div);
     pwm_set_wrap(slice_num, DUTY);
-    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(OUT_FREQ), DUTY / 2); // 50% duty cycle
+    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(OUT_PIN), DUTY / 2);
     pwm_set_enabled(slice_num, true);
 
     return;
@@ -94,7 +91,7 @@ void generate_square_wave(float freq) {
 float get_frequency(uint8_t mask) {
 	switch (mask) {
         case 0b11111111: return NOTE_C4;
-        case 0b11111110: return NOTE_D4; 
+        case 0b11111110: return NOTE_D4;
         case 0b11111100: return NOTE_E4;
         case 0b11111000: return NOTE_F4;
         case 0b11110000: return NOTE_G4;
