074099bdfa6d2e0087ba2acb403289515a83ee81
okmarcel <>
2025-11-04 23:32:29 +0100

    szpont

--- flute.c
+++ flute.c
@@ -8,16 +8,16 @@
 typedef unsigned int uint;
 
 // pinout
-#define BUTTON1     ((uint)0)
-#define BUTTON2     ((uint)1)
-#define BUTTON3     ((uint)2)
-#define BUTTON4     ((uint)3)
-#define BUTTON5     ((uint)4)
-#define BUTTON6     ((uint)5)
-#define BUTTON7     ((uint)6)
-#define BUTTON8     ((uint)7)
+#define BUTTON1     ((uint)15)
+#define BUTTON2     ((uint)14)
+#define BUTTON3     ((uint)13)
+#define BUTTON4     ((uint)12)
+#define BUTTON5     ((uint)11)
+#define BUTTON6     ((uint)10)
+#define BUTTON7     ((uint)9)
+#define BUTTON8     ((uint)8)
 #define ADC26       ((uint)26)
-#define OUT_PIN		((uint)27)
+#define OUT_PIN		((uint)16)
 
 // note frequencies
 #define NOTE_C4     261.63
@@ -33,62 +33,69 @@ typedef unsigned int uint;
 #define NOTE_AS4    466.16
 #define NOTE_B4     493.88
 #define NOTE_C5     523.25
+#define NOTE_D5     587.33
 
 // constants
-#define BUTTON_COUNT 8
+#define BUTTON_COUNT (int)8
 #define MIN_VOLUME 0
-#define MAX_VOLUME 4000
+#define MAX_VOLUME 4095
 #define TMP_FIXED 2000      // temp volume for testing
 #define DUTY 65535
 
-void generate_square_wave(float freq);
-float get_frequency(uint8_t mask);
+void generate_square_wave(const float freq, const int volume);
+float get_frequency(const uint8_t mask);
+int get_volume(void);
+float nonstandard_mask(const uint8_t mask);
 
 int main(void) {
-    uint buttons[BUTTON_COUNT] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, BUTTON8};
-    //bool buttons_pressed[BUTTON_COUNT] = {0};
-    int volume = 0;
+    const uint buttons[BUTTON_COUNT] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6, BUTTON7, BUTTON8};
 
-    for (register int i = 0; i < BUTTON_COUNT; i++) {
-        gpio_init(buttons[i]);
+    for (register int i = 0; i < BUTTON_COUNT; ++i) {
+		gpio_init(buttons[i]);
 		gpio_set_dir(buttons[i], GPIO_IN);
 		gpio_pull_up(buttons[i]);
     }
-    //adc_init();
-    //adc_gpio_init(ADC26);
-    //adc_select_input(0);
+    adc_init();
+    adc_gpio_init(ADC26);
+    adc_select_input(0);
+
 	gpio_init(OUT_PIN);
     gpio_set_function(OUT_PIN, GPIO_FUNC_PWM);
-    
+
+	float note_playing = 0.0f;
+	float last_note = 0.0f;
     while (true) {
 		uint8_t button_mask = 0;
-        for (register int i = 0; i < BUTTON_COUNT; i++) {
-            //buttons_pressed[i] = gpio_get(buttons[i]);
-			if (gpio_get(buttons[i])) {
+        for (register int i = 0; i < BUTTON_COUNT; ++i) {
+			if (!gpio_get(buttons[i])) {
 				button_mask |= (1 << i);
 			}
         }
-        //volume = adc_read();
-		generate_square_wave(get_frequency(button_mask));	
+		note_playing = get_frequency(button_mask);
+		if (note_playing == 0.0f) {
+			note_playing = last_note;
+		}
+		last_note = note_playing;
+		generate_square_wave(note_playing, get_volume());	
     }
 
     return EXIT_SUCCESS;
 }
 
-void generate_square_wave(float freq) {
-    uint slice_num = pwm_gpio_to_slice_num(OUT_PIN);
-    float clk_div = (float)125000000 / freq / (DUTY + 1);
-    if (clk_div < 1) clk_div = 1;
+void generate_square_wave(const float freq, const int volume) {
+    const uint slice_num = pwm_gpio_to_slice_num(OUT_PIN);
+    float clk_div = 125000000.0f / freq / (DUTY + 1);
+    if (clk_div < 1.0f) clk_div = 1.0f;
     
     pwm_set_clkdiv(slice_num, clk_div);
     pwm_set_wrap(slice_num, DUTY);
-    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(OUT_PIN), DUTY / 2);
+    pwm_set_chan_level(slice_num, pwm_gpio_to_channel(OUT_PIN), volume);
     pwm_set_enabled(slice_num, true);
 
     return;
 }
 
-float get_frequency(uint8_t mask) {
+float get_frequency(const uint8_t mask) {
 	switch (mask) {
         case 0b11111111: return NOTE_C4;
         case 0b11111110: return NOTE_D4;
@@ -98,7 +105,26 @@ float get_frequency(uint8_t mask) {
         case 0b11100000: return NOTE_A4;
         case 0b11000000: return NOTE_B4;
         case 0b10000000: return NOTE_C5;
-        default: return 0.0f; // no sound
+        case 0b00000000: return NOTE_D5;
+		default: return nonstandard_mask(mask);
 	}
+}
+
+int get_volume(void) {
+	int adc_value = adc_read();
+	return (int)((float)adc_value / MAX_VOLUME * (float)DUTY / 2);
+}
+
+float nonstandard_mask(const uint8_t mask) {
+	uint8_t check_mask = 0b10000000;
+	for (register int i = 0; i < BUTTON_COUNT; ++i) {
+		if ((mask & check_mask) != check_mask) {
+			check_mask = 0b11111111;
+			check_mask = check_mask << (BUTTON_COUNT - i);
+			break;
+		}
+		check_mask = check_mask >> 1;
+	}
+	return get_frequency(check_mask);
 }
 
