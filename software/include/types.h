214835a847db08cb7c36a90516b34516d8310251
Jakub Piela <173794188+RavenCS1@users.noreply.github.com>
2025-12-23 10:46:56 +0100

    Add central types.h with PROJECT_VARIANT macros

---
+++ software/include/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// Should be defined through CMAKE
+#define DEBUG_MODE 0
+#define PROJECT_TYPE PROJECT_INT
+
+#if PROJECT_TYPE == PROJECT_INT
+    #define project_type     int
+#elif PROJECT_TYPE == PROJECT_FLOAT
+    #define project_type     float
+#endif
+
+#endif
